c9ca3b5da057d9b886e3d7e36247cf1f1e4fda83
Nick Bueltel <nickbueltel@gmail.com>
2019-09-04 15:34:00 -0500

    revert

---
+++ src/pages/nav.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import {Menu, Container } from 'semantic-ui-react'
+import HomePage from '../pages/home'
+import FoodMenu from '../pages/menu'
+import About from '../pages/about'
+import Contact from '../pages/contact'
+
+
+class NavBar extends React.Component{
+    state = { activeItem: "home"}
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+    
+    
+    render(){
+        const { activeItem } = this.state
+        return(
+                <div>
+        
+                <Menu>
+                    <Menu.Item name="home" active={activeItem === 'home'} onClick={this.handleItemClick} > 
+                        Home
+                    </Menu.Item>
+
+                    
+                    
+                    <Menu.Item name="about" active={activeItem === 'about'} onClick={this.handleItemClick}>
+                        About
+                    </Menu.Item>
+                    
+                    <Menu.Item name="contact" active={activeItem === 'contact'} onClick={this.handleItemClick}>
+                        Directions
+                    </Menu.Item>
+                    <Menu.Item href="http://www.toasttab.com/andaman-southern-thai-cuisine">
+                        Order Online
+                    </Menu.Item>
+                </Menu>
+                <Container>
+                    {this.state.activeItem === "home" ? <HomePage/> : null}
+                    {this.state.activeItem === "about" ? <About/> : null}
+                    {this.state.activeItem === "contact" ? <Contact/> : null}
+
+
+                </Container>
+
+            
+                </div>
+        )
+    }
+}
+
+export default NavBar
